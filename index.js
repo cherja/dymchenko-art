0ea4033d92d655d05006d95f50b325390011a575
Виталий Чернявский <4irik91@mail.ru>
2017-06-24 18:14:12 +0400

    Навигация
    Горизонтальная навигация,анимированная рамка,при 980-680 виде уменьшается шрифт,отступы,толщина рамки,при 320-679 появляется бургер с выпадающей навигацией

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("is-active");
 });
+
+var header = document.querySelector(".header");
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+  header.classList.toggle("active");
+});
