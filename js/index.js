e363857c6703d3caa5a19ba31ec59f071b96576e
Виталий Чернявский <4irik91@mail.ru>
2017-06-30 11:54:58 +0400

    Сделал все в квадратном стиле,оживил кнопку наверх=))

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ baguetteBox.run('.gallery', {
   animation: 'fadeIn',
   noScrollbars: true
 });
+
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+
+  $('.scrollup').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+
+});
