e719b601256654518e019206de8dbf5cbdb807b1
jonkofee <jonkofee@icloud.com>
2017-07-03 13:48:43 +0300

    #1 fix anchor animate

--- js/index.js
+++ js/index.js
@@ -20,7 +20,8 @@ $(document).ready(function() {
   $('a[href^="#"]').click(function() {
     elementClick = $(this).attr("href");
     destination = $(elementClick).offset().top;
-    if ($.browser.safari) {
+
+    if (navigator.userAgent.indexOf("Safari") > -1) {
       $('body').animate({
         scrollTop: destination
       }, 1100);
