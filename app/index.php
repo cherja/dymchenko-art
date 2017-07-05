3f9529c395da1ea2ecc8b8d04228e918997b8120
jonkofee <jonkofee@icloud.com>
2017-07-05 12:30:13 +0300

    Минус гугл аналитика

--- app/index.php
+++ app/index.php
@@ -60,19 +60,6 @@
   <!-- Twitter -->
   <meta name="twitter:card" content="summary" />
   <!-- end Twitter -->
-
-  <!-- GA -->
-  <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-102034595-1', 'auto');
-  ga('send', 'pageview');
-
-  </script>
-  <!-- end GA -->
 </head>
 
 <body>
