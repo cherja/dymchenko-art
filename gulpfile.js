ab1c59ec86f97464011b8d1cbb05d3ad7c07fdee
jonkofee <jonkofee@icloud.com>
2017-08-06 12:11:52 +0300

    favicons

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,7 @@ var gulp = require('gulp'),
   tinypng = require('gulp-tinypng-compress'), //сжатие изображений
   ga = require('gulp-ga'), //гугл аналитика
   strip = require('gulp-strip-comments'), //удаление комментариев
-  autoprefixer = require('gulp-autoprefixer'),
-  favicons = require('gulp-favicons')
+  autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('clear', function() {
   return del(['dist']);
@@ -47,22 +46,6 @@ gulp.task('images', function() {
     .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('favicons', function() {
-  return gulp.src("app/icon.svg")
-    .pipe(favicons({
-      appName: "Dymchenko Art",
-      appDescription: "Photografer",
-      developerName: "Cherja",
-      developerURL: "http://cherja.ru/",
-      path: "favicons/",
-      url: "http://dymchenko-art.ru/",
-      orientation: "portrait",
-      html: "dist/index.html",
-      lang: "ru_RU"
-    }))
-    .pipe(gulp.dest("dist/favicons"));
-});
-
 gulp.task('ga', function() {
   return gulp.src('dist/index.html')
     .pipe(ga({
@@ -73,4 +56,4 @@ gulp.task('ga', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', gulp.series('clear', 'html', 'js', 'css', 'images', 'favicons', 'ga'));
+gulp.task('build', gulp.series('clear', 'html', 'js', 'css', 'images', 'ga'));
